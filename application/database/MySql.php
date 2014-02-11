90a10bb5bf6ddfce9a17e0d3487827fd440985ea
BreezeDust <BreezeDust@qq.com>
2014-02-11 22:24:52 +0800

    +

--- application/database/MySql.php
+++ application/database/MySql.php
@@ -9,8 +9,8 @@ class MySql {
 	public function initConnect(){
 		$host = 'localhost';
 		$port = '3306';
-		$user = 'root';
-		$pwd = '7252072520';
+		$user = 'ts';
+		$pwd = '123456';
 		$dbname = 'BreezeDustWEB';
 		$this->con = @mysql_connect("{$host}:{$port}",$user,$pwd,true);
 		if(!$this->con) {
