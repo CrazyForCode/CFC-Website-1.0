cd1b083e05b7d6ce8db3b00b08bd82fe530b8162
BreezeDust <BreezeDust@qq.com>
2014-02-13 21:24:06 +0800

    index style2 and other html

---
+++ js/header.js
@@ -0,0 +1,22 @@
+﻿function NAVhover(obj) {
+    var nodes = obj.childNodes;
+    for (var i = 0; i < obj.childNodes.length; i++) {
+        if (nodes[i].className == "NAVhover") {
+            obj = nodes[i];
+            break;
+        }
+    }
+     obj.style.display = "block";
+}
+function NAVout(obj) {
+    var nodes = obj.childNodes;
+    for (var i = 0; i < obj.childNodes.length; i++) {
+        if (nodes[i].className == "NAVhover") {
+            obj = nodes[i];
+            break;
+        }
+    }
+    obj.style.display = "none";
+}
+
+
---
+++ js/indexHead.js
@@ -0,0 +1,35 @@
+﻿function NAVhover(obj) {
+    var nodes = obj.childNodes;
+    for (var i = 0; i < obj.childNodes.length; i++) {
+        if (nodes[i].className == "NAVhover") {
+            obj = nodes[i];
+            break;
+        }
+    }
+     obj.style.display = "block";
+}
+function NAVout(obj) {
+    var nodes = obj.childNodes;
+    for (var i = 0; i < obj.childNodes.length; i++) {
+        if (nodes[i].className == "NAVhover") {
+            obj = nodes[i];
+            break;
+        }
+    }
+    obj.style.display = "none";
+}
+
+
+$(function () {
+    $("#headers").mouseenter(
+       function () {
+           $("#headersBG").animate({ height: '150px' });
+       }
+        );
+    $("#headers").mouseleave(
+       function () {
+           $("#headersBG").animate({ height: '0px' });
+       }
+        );
+});
+
