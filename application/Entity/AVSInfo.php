023d9b9dace5e45c7e131175812539d04226ad62
lixin-v <527381107@qq.com>
2014-03-08 15:10:36 +0800

    avsinfo.php

--- application/Entity/AVSInfo.php
+++ application/Entity/AVSInfo.php
@@ -1,14 +1,38 @@
 <?php
 class AVSInfo {
-	public $avID;
-	public $avName;
-	public $avIMG;
+	public $AVID;
 	public $avTag;
-	public $avAge;
-	public $avHight;
+	public $avOutIMG;
+	public $avInIMG;
+	public $avName;
+	public $avSex;
+	public $avBirth;
+	public $avNationality;
+	public $avNation;
+	public $avProvince;
+	public $avIDcard;
+	public $avAdrr;
+	public $avPostCode;
+	public $avWeiBo;
+	public $avQQEMAIL;
+	public $avMobile;
+	public $avPhone;
+	public $avHieght;
 	public $avWeight;
-	public $av3w;
+	public $avFacePaint;
+	public $avColor;
+	public $avEyeColor;
+	public $avShoeSize;
+	public $avSH;
+	public $avButs;
+	public $avWaist;
+	public $avHips;
+	public $avCup;
+	public $avStyle;
+	public $avWorkTime;
+	public $avISAgree;
 	public $avContent;
 	public $avDate;
+
 }
 ?>
